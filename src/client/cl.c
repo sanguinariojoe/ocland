cc8f1e9e1940fabf71cd11f3502236e92a20f9ae
Jose Luis Cercos Pita <jlcercos@gmail.com>
2013-02-23 14:46:26 +0100

    Added clEnqueueNativeKernel method

--- src/client/cl.c
+++ src/client/cl.c
@@ -812,6 +812,22 @@ clEnqueueTask(cl_command_queue   command_queue ,
                                   event);
 }
 
+CL_API_ENTRY cl_int CL_API_CALL
+clEnqueueNativeKernel(cl_command_queue   command_queue ,
+                      void (CL_CALLBACK *user_func)(void *),
+                      void *             args ,
+                      size_t             cb_args ,
+                      cl_uint            num_mem_objects ,
+                      const cl_mem *     mem_list ,
+                      const void **      args_mem_loc ,
+                      cl_uint            num_events_in_wait_list ,
+                      const cl_event *   event_wait_list ,
+                      cl_event *         event) CL_API_SUFFIX__VERSION_1_0
+{
+    /// Native kernels cannot be supported by remote applications
+    return CL_INVALID_OPERATION;
+}
+
 #ifdef CL_API_SUFFIX__VERSION_1_1
 CL_API_ENTRY cl_mem CL_API_CALL
 clCreateSubBuffer(cl_mem                    buffer ,
