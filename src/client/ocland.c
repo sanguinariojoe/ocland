7fbcef0a13a1b38a5921519e2a882c3bdbf3921d
Jose Luis Cercos Pita <jlcercos@gmail.com>
2013-11-10 17:50:36 +0100

    Updated clEnqueueWriteImage

--- src/client/ocland.c
+++ src/client/ocland.c
@@ -2464,14 +2464,25 @@ cl_int oclandEnqueueWriteImage(cl_command_queue     command_queue ,
                                cl_event *           event)
 {
     cl_int flag;
+    cl_uint i;
     unsigned int comm = ocland_clEnqueueWriteImage;
     cl_bool want_event = CL_FALSE;
     if(event) want_event = CL_TRUE;
     size_t cb = region[2]*slice_pitch + region[1]*row_pitch + region[0]*element_size;
     // Get the server
-    int *sockfd = getShortcut(command_queue);
+    int *sockfd = command_queue->socket;
     if(!sockfd){
         return CL_INVALID_COMMAND_QUEUE;
+    }
+    // Change the events from the local references to the remote ones
+    cl_event *events_wait = NULL;
+    if(num_events_in_wait_list){
+        events_wait = (cl_event*)malloc(num_events_in_wait_list*sizeof(cl_event));
+        if(!events_wait){
+            return CL_OUT_OF_HOST_MEMORY;
+        }
+        for(i=0;i<num_events_in_wait_list;i++)
+            events_wait[i] = event_wait_list[i]->ptr;
     }
     // Send the command data
     Send(sockfd, &comm, sizeof(unsigned int), MSG_MORE);
@@ -2484,15 +2495,17 @@ cl_int oclandEnqueueWriteImage(cl_command_queue     command_queue ,
     Send(sockfd, &slice_pitch, sizeof(size_t), MSG_MORE);
     Send(sockfd, &element_size, sizeof(size_t), MSG_MORE);
     Send(sockfd, &want_event, sizeof(cl_bool), MSG_MORE);
-    if( (num_events_in_wait_list) || (blocking_write) ){
+    int ending = 0;
+    if(blocking_write) ending = MSG_MORE;
+    if(num_events_in_wait_list){
         Send(sockfd, &num_events_in_wait_list, sizeof(cl_uint), MSG_MORE);
+        Send(sockfd, events_wait, num_events_in_wait_list*sizeof(cl_event), ending);
     }
     else{
-        Send(sockfd, &num_events_in_wait_list, sizeof(cl_uint), 0);
+        Send(sockfd, &num_events_in_wait_list, sizeof(cl_uint), ending);
     }
+    free(events_wait); events_wait=NULL;
     if(blocking_write){
-        if(num_events_in_wait_list)
-            Send(sockfd, event_wait_list, num_events_in_wait_list*sizeof(cl_event), MSG_MORE);
         dataPack in, out;
         in.size = cb;
         in.data = ptr;
@@ -2500,10 +2513,6 @@ cl_int oclandEnqueueWriteImage(cl_command_queue     command_queue ,
         Send(sockfd, &(out.size), sizeof(size_t), MSG_MORE);
         Send(sockfd, out.data, out.size, 0);
         free(out.data); out.data = NULL;
-    }
-    else{
-        if(num_events_in_wait_list)
-            Send(sockfd, event_wait_list, num_events_in_wait_list*sizeof(cl_event), 0);
     }
     // Receive the answer
     Recv(sockfd, &flag, sizeof(cl_int), MSG_WAITALL);
