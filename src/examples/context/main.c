b9245539ed58681b9bc20a75ccb12d0130f9674e
J.L. Cercos-Pita <jlcercos@gmail.com>
2014-12-13 14:28:29 +0100

    Added a contexts example

---
+++ src/examples/context/main.c
@@ -0,0 +1,235 @@
+/*
+ *  This file is part of ocland, a free cloud OpenCL interface.
+ *  Copyright (C) 2012  Jose Luis Cercos Pita <jl.cercos@upm.es>
+ *
+ *  ocland is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ocland is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ocland.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <CL/opencl.h>
+
+/// OpenCL program source code
+const char* program_src = "__kernel void test(__global float* x, \n\
+                                              __global float* y, \n\
+                                              __global float* z, \n\
+                                              unsigned int i0,   \n\
+                                              unsigned int N)    \n\
+{                                                                \n\
+    unsigned int i = get_global_id(0);                           \n\
+    if(i >= N) return;                                           \n\
+    i+=i0;                                                       \n\
+    z[i]=x[i]*y[i];                                              \n\
+}";
+
+int main(int argc, char *argv[])
+{
+    unsigned int i,j,k;
+    char buffer[1025]; strcpy(buffer, "");
+    cl_uint num_entries = 0, num_platforms = 0;
+    cl_platform_id *platforms = NULL;
+    cl_int flag;
+    // Get number of platforms
+    flag = clGetPlatformIDs(0, NULL, &num_platforms);
+    if(flag != CL_SUCCESS){
+        printf("Error getting number of platforms\n");
+        if(flag == CL_INVALID_VALUE)
+            printf("\tCL_INVALID_VALUE\n");
+        if(flag == CL_OUT_OF_HOST_MEMORY)
+            printf("\tCL_OUT_OF_HOST_MEMORY\n");
+        return EXIT_FAILURE;
+    }
+    if(!num_platforms){
+        printf("No OpenCL platforms found...\n");
+        return EXIT_FAILURE;
+    }
+    printf("%u platforms found...\n", num_platforms);
+    // Build platforms array
+    num_entries = num_platforms;
+    platforms   = (cl_platform_id*)malloc(num_entries*sizeof(cl_platform_id));
+    // Get platforms array
+    flag = clGetPlatformIDs(num_entries, platforms, &num_platforms);
+    if(flag != CL_SUCCESS){
+        printf("Error getting platforms\n");
+        if(flag == CL_INVALID_VALUE)
+            printf("\tCL_INVALID_VALUE\n");
+        if(flag == CL_OUT_OF_HOST_MEMORY)
+            printf("\tCL_OUT_OF_HOST_MEMORY\n");
+        return EXIT_FAILURE;
+    }
+    // Create the devices
+    for(i=0;i<num_platforms;i++){
+        printf("Platform %u...\n", i);
+        clGetPlatformInfo(platforms[i],CL_PLATFORM_NAME,1025*sizeof(char),buffer, NULL);
+        printf("\t%s\n", buffer);
+        // Get number of devices
+        num_entries = 0;
+        cl_uint num_devices = 0;
+        cl_device_id *devices = NULL;
+        cl_event *events = NULL;
+        flag = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, num_entries, devices, &num_devices);
+        if( (flag != CL_SUCCESS) && (flag != CL_DEVICE_NOT_FOUND) ) {
+            printf("Error getting number of devices\n");
+            if(flag == CL_INVALID_PLATFORM)
+                printf("\tCL_INVALID_PLATFORM\n");
+            if(flag == CL_INVALID_DEVICE_TYPE)
+                printf("\tCL_INVALID_DEVICE_TYPE\n");
+            if(flag == CL_INVALID_VALUE)
+                printf("\tCL_INVALID_VALUE\n");
+            if(flag == CL_OUT_OF_RESOURCES)
+                printf("\tCL_OUT_OF_RESOURCES\n");
+            if(flag == CL_OUT_OF_HOST_MEMORY)
+                printf("\tCL_OUT_OF_HOST_MEMORY\n");
+            return EXIT_FAILURE;
+        }
+        if( (!num_devices) || (flag == CL_DEVICE_NOT_FOUND) ){
+            printf("\tWithout devices.\n");
+            continue;
+        }
+        // Build devices array
+        num_entries = num_devices;
+        devices     = (cl_device_id*)malloc(num_entries*sizeof(cl_device_id));
+        events      = (cl_event*)malloc(num_entries*sizeof(cl_event));
+        // Get devices array
+        flag = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, num_entries, devices, &num_devices);
+        if( (flag != CL_SUCCESS) && (flag != CL_DEVICE_NOT_FOUND) ) {
+            printf("Error getting number of devices\n");
+            if(flag == CL_INVALID_PLATFORM)
+                printf("\tCL_INVALID_PLATFORM\n");
+            if(flag == CL_INVALID_DEVICE_TYPE)
+                printf("\tCL_INVALID_DEVICE_TYPE\n");
+            if(flag == CL_INVALID_VALUE)
+                printf("\tCL_INVALID_VALUE\n");
+            if(flag == CL_OUT_OF_RESOURCES)
+                printf("\tCL_OUT_OF_RESOURCES\n");
+            if(flag == CL_OUT_OF_HOST_MEMORY)
+                printf("\tCL_OUT_OF_HOST_MEMORY\n");
+            return EXIT_FAILURE;
+        }
+        // Create a context
+        cl_context_properties contextProperties[3] = {
+            CL_CONTEXT_PLATFORM,
+            (cl_context_properties)platforms[i],
+            0
+        };
+        cl_context context = clCreateContext(contextProperties, num_devices, devices, NULL, NULL, &flag);
+        if(flag != CL_SUCCESS) {
+            printf("Error building context\n");
+            if(flag == CL_INVALID_PLATFORM)
+                printf("\tCL_INVALID_PLATFORM\n");
+            if(flag == CL_INVALID_VALUE)
+                printf("\tCL_INVALID_VALUE\n");
+            if(flag == CL_INVALID_DEVICE)
+                printf("\tCL_INVALID_DEVICE\n");
+            if(flag == CL_DEVICE_NOT_AVAILABLE)
+                printf("\tCL_DEVICE_NOT_AVAILABLE\n");
+            if(flag == CL_OUT_OF_HOST_MEMORY)
+                printf("\tCL_OUT_OF_HOST_MEMORY\n");
+            return EXIT_FAILURE;
+        }
+        printf("\tBuilt context with %u devices!\n", num_devices);
+
+        // Print context data
+        cl_uint reference_count;
+        clGetDeviceInfo(context,
+                        CL_CONTEXT_REFERENCE_COUNT,
+                        sizeof(cl_uint),
+                        &reference_count,
+                        NULL);
+        printf("\t\tREFERENCE COUNT: %u\n", reference_count);
+        size_t devices_size;
+        clGetContextInfo(context,
+                         CL_CONTEXT_DEVICES,
+                         0,
+                         NULL,
+                         &devices_size);
+        printf("\t\tNUMBER OF DEVICES: %lu\n", devices_size / sizeof(cl_device_id));
+        if(num_devices != devices_size / sizeof(cl_device_id)){
+            printf("\t\tSO IT FAILED!\n");
+            continue;
+        }
+        cl_device_id *devices_helper = (cl_device_id*)malloc(devices_size);
+        clGetContextInfo(context,
+                         CL_CONTEXT_DEVICES,
+                         devices_size,
+                         devices_helper,
+                         NULL);
+        printf("\t\tDEVICES: ");
+        int OK = 1;
+        for(j = 0; j < devices_size / sizeof(cl_device_id); j++){
+            if(devices_helper[j] != devices[j]){
+                OK = 0;
+            }
+        }
+        free(devices_helper); devices_helper = NULL;
+        if(OK){
+            printf("OK\n");
+        }
+        else{
+            printf("FAIL\n");
+            continue;
+        }
+        size_t props_size;
+        clGetContextInfo(context,
+                         CL_CONTEXT_PROPERTIES,
+                         0,
+                         NULL,
+                         &props_size);
+        printf("\t\tNUMBER OF PROPERTIES: %lu\n", props_size / sizeof(cl_context_properties));
+        if(3 != props_size / sizeof(cl_context_properties)){
+            printf("\t\tSO IT FAILED!\n");
+            continue;
+        }
+        cl_context_properties *props_helper = (cl_context_properties*)malloc(props_size);
+        clGetContextInfo(context,
+                         CL_CONTEXT_PROPERTIES,
+                         props_size,
+                         props_helper,
+                         NULL);
+        printf("\t\tPROPERTIES: ");
+        OK = 1;
+        for(j = 0; j < props_size / sizeof(cl_context_properties); j++){
+            if(props_helper[j] != contextProperties[j]){
+                OK = 0;
+            }
+        }
+        free(props_helper); props_helper = NULL;
+        if(OK){
+            printf("OK\n");
+        }
+        else{
+            printf("FAIL\n");
+            continue;
+        }
+
+        flag = clReleaseContext(context);
+        if(flag != CL_SUCCESS) {
+            printf("Error releasing context\n");
+            if(flag == CL_INVALID_CONTEXT)
+                printf("\tCL_INVALID_CONTEXT\n");
+            if(flag == CL_OUT_OF_RESOURCES)
+                printf("\tCL_OUT_OF_RESOURCES\n");
+            if(flag == CL_OUT_OF_HOST_MEMORY)
+                printf("\tCL_OUT_OF_HOST_MEMORY\n");
+            return EXIT_FAILURE;
+        }
+        printf("\tRemoved context.\n");
+        if(devices) free(devices); devices=NULL;
+    }
+    if(platforms) free(platforms); platforms=NULL;
+    return EXIT_SUCCESS;
+}
