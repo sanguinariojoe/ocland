9f20f95741811fe77489f601dee0557f46de011f
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2015-02-10 14:44:55 +0100

    Removed useless command queues from buffer example

--- src/examples/buffer/main.c
+++ src/examples/buffer/main.c
@@ -315,28 +315,6 @@ int main(int argc, char *argv[])
         }
         printf("\tBuilt context with %u devices!\n", num_devices);
 
-        // Create the command queues for each device
-        cl_command_queue *queues = (cl_command_queue*)malloc(
-            num_devices * sizeof(cl_command_queue));
-        for(j = 0; j < num_devices; j++){
-            queues[j] = clCreateCommandQueue(context, devices[j], 0, &flag);
-            if(flag != CL_SUCCESS) {
-                printf("Error building command queue\n");
-                if(flag == CL_INVALID_CONTEXT)
-                    printf("\tCL_INVALID_CONTEXT\n");
-                if(flag == CL_INVALID_DEVICE)
-                    printf("\tCL_INVALID_DEVICE\n");
-                if(flag == CL_INVALID_VALUE)
-                    printf("\tCL_INVALID_VALUE\n");
-                if(flag == CL_INVALID_QUEUE_PROPERTIES)
-                    printf("\tCL_INVALID_QUEUE_PROPERTIES\n");
-                if(flag == CL_OUT_OF_HOST_MEMORY)
-                    printf("\tCL_OUT_OF_HOST_MEMORY\n");
-                return EXIT_FAILURE;
-            }
-            printf("\tBuilt command queue (device %u / %u)!\n", j, num_devices-1);
-        }
-
         // Create a buffer
         unsigned int n = 1000000;
         cl_float *hx = (cl_float*)malloc(n * sizeof(cl_float));
@@ -478,18 +456,6 @@ int main(int argc, char *argv[])
         if(x) clReleaseMemObject(x); x=NULL;
         if(hx) free(hx); hx=NULL;
         printf("\tRemoved the memory buffer.\n");
-
-        for(j = 0; j < num_devices; j++){
-            flag = clReleaseCommandQueue(queues[j]);
-            if(flag != CL_SUCCESS) {
-                printf("Error releasing command queue\n");
-                if(flag == CL_INVALID_COMMAND_QUEUE)
-                    printf("\tCL_INVALID_COMMAND_QUEUE\n");
-                return EXIT_FAILURE;
-            }
-            printf("\tRemoved command queue (device %u / %u).\n", j, num_devices-1);
-        }
-        if(queues) free(queues); queues=NULL;
 
         flag = clReleaseContext(context);
         if(flag != CL_SUCCESS) {
