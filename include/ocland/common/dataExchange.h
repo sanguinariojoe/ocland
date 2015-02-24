e76e6ef1d775d9b560fcc29504aec1c914236361
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2015-02-24 12:49:40 +0100

    Improved the way data exchangers are checking the connection

--- include/ocland/common/dataExchange.h
+++ include/ocland/common/dataExchange.h
@@ -19,34 +19,48 @@
 #ifndef DATAEXCHANGE_H_INCLUDED
 #define DATAEXCHANGE_H_INCLUDED
 
-/** Returns the last socket error detected
- * @return Error detected.
- */
-const char* SocketsError();
-
-/** recv method reimplementation. This reimplementation will expect that received data matchs
- * with request data on length, if not (errors or mismatch) connection will closed (and socket
- * conviniently set to -1).
+/** @brief recv method reimplementation.
+ *
+ * This reimplementation will expect that received data matches with requested
+ * data on length, otherwise connection will be closed (and socket conveniently
+ * set to -1).
  * @param socket Specifies the socket file descriptor.
  * @param buffer Points to a buffer where the message should be stored.
- * @param length Specifies the length in bytes of the buffer pointed to by the buffer argument.
+ * @param length Specifies the length in bytes of the buffer pointed to by the
+ * buffer argument.
  * @param flags Specifies the type of message reception.
- * @return Upon successful completion, Recv() shall return the length of the message in bytes.
- * If no messages are available to be received and the peer has performed an orderly shutdown,
- * Recv() shall return 0. Otherwise, -1 shall be returned and errno set to indicate the error.
+ * @return Upon successful completion, the length of the message in bytes.
+ * 0 if no messages are available to be received and the peer has asked for the
+ * connection close.
+ * -1 in case of errors.
+ * @note if DATA_EXCHANGE_VERBOSE is defined the function is printing the error
+ * message in the screen.
  */
-ssize_t Recv(int *socket, void *buffer, size_t length, int flags);
+ssize_t Recv(int *socket, void *buffer, size_t length, int flags, int verbose);
 
-/** send method reimplementation. This reimplementation will expect that sent data matchs
- * with request data on length, if not (errors or mismatch) connection will closed (and socket
- * conviniently set to -1).
+/** @brief send method reimplementation.
+ *
+ * This reimplementation will expect that sent data matches with the requested
+ * by length, otherwise connection will be closed (and socket conveniently
+ * set to -1).
  * @param socket Specifies the socket file descriptor.
  * @param buffer Points to the buffer containing the message to send.
  * @param length Specifies the length of the message in bytes.
  * @param flags Specifies the type of message transmission.
- * @return Upon successful completion, Send() shall return the number of bytes sent. Otherwise,
- * -1 shall be returned and errno set to indicate the error.
+ * @return Upon successful completion, the length of the message in bytes.
+ * -1 in case of errors.
+ * @note if DATA_EXCHANGE_VERBOSE is defined the function is printing the error
+ * message in the screen.
  */
 ssize_t Send(int *socket, const void *buffer, size_t length, int flags);
+
+#define ssend(socket, buffer, length, flags) {printf("[line %d]: %s...\n", __LINE__, __func__); fflush(stdout);}
+    #define VERBOSE_OUT(flag) {printf("\t%s -> %s\n", __func__, OpenCLError(flag)); fflush(stdout);}
+    #define VERBOSE(...) {printf(__VA_ARGS__); fflush(stdout);}
+#else
+    #define VERBOSE_IN()
+    #define VERBOSE_OUT(flag)
+    #define VERBOSE(...)
+#endif
 
 #endif // DATAEXCHANGE_H_INCLUDED
