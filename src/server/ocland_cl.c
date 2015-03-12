9287193eccf47a55a3c1283bf8af5703d61b8894
Timur Magomedov <tim239@yandex.ru>
2015-03-12 17:18:35 +0300

    Increased performance for clEnqueueNDRangeKernel
    Wrong MSG_MORE flag fixed

--- src/server/ocland_cl.c
+++ src/server/ocland_cl.c
@@ -2786,9 +2786,10 @@ int ocland_clEnqueueNDRangeKernel(int* clientfd, validator v)
         return 1;
     }
     // Answer to the client
-    Send(clientfd, &flag, sizeof(cl_int), MSG_MORE);
+    int send_flags = want_event ? MSG_MORE : 0;
+    Send(clientfd, &flag, sizeof(cl_int), send_flags);
     if(want_event){
-        Send(clientfd, &event, sizeof(ocland_event), MSG_MORE);
+        Send(clientfd, &event, sizeof(ocland_event), 0);
         registerEvent(v,event);
         event->status = CL_COMPLETE;
     }
